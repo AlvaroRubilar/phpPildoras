cbab5cc144f008ca6f88619bafb9fb5acb1773cd
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-24 14:44:14 -0300

    video 8 operador ternario

--- trabajoString.php
+++ trabajoString.php
@@ -20,7 +20,7 @@
 $variable1 = "casa";
 $variable2 = "CASA";
 $resultado = strcasecmp($variable1,$variable2);
-echo "El resultado es $resultado";
+echo $resultado? "No coinciden" : "Coinciden";
 
 
 
