788f8291c5b0ce9c4e2feb1de2897ad9df3ee676
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-25 18:27:14 -0300

    video 12 definir función calcular

--- calculadora.php
+++ calculadora.php
@@ -6,21 +6,24 @@ if (isset($_POST["button"])) {
     $numero1 = $_POST["num1"];
     $numero2 = $_POST["num2"];
     $operacion = $_POST["operacion"];
+    calcular($operacion);
+}
 
-
-    if (!strcmp("Suma", $operacion)) {
+function calcular($calculo){
+    global $numero1,$numero2;
+    if (!strcmp("Suma", $calculo)) {
         echo "El resultado de la suma entre $numero1 y $numero2 es " . ($numero1 + $numero2);
     }
-    if (!strcmp("Resta", $operacion)) {
+    if (!strcmp("Resta", $calculo)) {
         echo "El resultado de la resta entre $numero1 y $numero2 es " . ($numero1 - $numero2);
     }
-    if (!strcmp("Multiplicación", $operacion)) {
+    if (!strcmp("Multiplicación", $calculo)) {
         echo "El resultado de la multiplicación entre $numero1 y $numero2 es " . ($numero1 * $numero2);
     }
-    if (!strcmp("División", $operacion)) {
+    if (!strcmp("División", $calculo)) {
         echo "El resultado de la división entre $numero1 y $numero2 es " . ($numero1 / $numero2);
     }
-    if (!strcmp("Módulo", $operacion)) {
+    if (!strcmp("Módulo", $calculo)) {
         echo "El resto al dividir $numero1 entre $numero2 es " . ($numero1 % $numero2);
     }
 
