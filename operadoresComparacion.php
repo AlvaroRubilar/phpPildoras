d7a371c7f4cc5432d5766e02d1259adc7d0e8d7f
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-25 08:23:09 -0300

    video 9 comparar si el contenido es igual

---
+++ operadoresComparacion.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$variable1=8;
+$variable2="8";
+$variable3="Alvaro";
+
+echo $variable1==$variable2? "Son iguales":"No son iguales";
+
+?>
+</body>
+</html>
