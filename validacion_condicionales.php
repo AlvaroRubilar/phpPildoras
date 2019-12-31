e54e1d0afe5b72b83a52ec7b25323a02ee1bb3b3
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-30 23:06:32 -0300

    video 17  switch usuarios

--- validacion_condicionales.php
+++ validacion_condicionales.php
@@ -2,34 +2,51 @@
     h1 {
         text-align: center;
     }
-
     table {
         background-color: #FFC;
         padding: 5px;
         border: #666 5px solid;
     }
-
     .no_validado {
         font-size: 18px;
         color: #F00;
         font-weight: bold;
     }
-
     .validado {
         font-size: 18px;
         color: #0C3;
         font-weight: bold;
     }
-
-
 </style>
 
 <?php
 if (isset($_POST["enviando"])) {
+    $edad = $_POST["edad_usuario"];
     $nombre = $_POST["nombre_usuario"];
-    $contra = $_POST["contra"];
-    $resultado = $nombre == "Alvaro" && $contra == "1234" ? "Puedes acceder" : "No puedes acceder";
-    echo $resultado;
-}
+//    if ($edad <= 18) {
+//        echo "Eres menor de edad";
+//    } else if ($edad <= 40) {
+//        echo "Eres joven";
+//    } else if ($edad <= 65) {
+//        echo "Eres maduro";
+//    } else {
+//        echo "Cuídate";
+//    }
+    switch ($nombre){
 
-    ?>
+        case "Juan":
+            echo "Usuario autorizado, hola $nombre";
+            break;
+        case "María":
+            echo "Usuario autorizado, hola $nombre";
+            break;
+        case "Pedro":
+            echo "Usuario autorizado, hola $nombre";
+            break;
+        default:
+            echo "Usuario no autorizado";
+
+
+    }
+}
+?>
