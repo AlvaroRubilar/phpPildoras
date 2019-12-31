736412cc847c7a980c995ecbfb6deedf818338aa
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-30 23:22:10 -0300

    video 17  switch(true) edad

--- validacion_condicionales.php
+++ validacion_condicionales.php
@@ -2,19 +2,16 @@
     h1 {
         text-align: center;
     }
-
     table {
         background-color: #FFC;
         padding: 5px;
         border: #666 5px solid;
     }
-
     .no_validado {
         font-size: 18px;
         color: #F00;
         font-weight: bold;
     }
-
     .validado {
         font-size: 18px;
         color: #0C3;
@@ -24,32 +21,23 @@
 
 <?php
 if (isset($_POST["enviando"])) {
-    $contra = $_POST["contra"];
+    $edad = $_POST["edad_usuario"];
     $nombre = $_POST["nombre_usuario"];
-//    if ($edad <= 18) {
-//        echo "Eres menor de edad";
-//    } else if ($edad <= 40) {
-//        echo "Eres joven";
-//    } else if ($edad <= 65) {
-//        echo "Eres maduro";
-//    } else {
-//        echo "Cuídate";
-//    }
-    switch (true) {
-
-        case $nombre == "Juan" && $contra == "1234":
-            echo "Usuario autorizado, hola $nombre";
-            break;
-        case $nombre == "María" && $contra == "5555":
-            echo "Usuario autorizado, hola $nombre";
-            break;
-        case $nombre == "Pedro" && $contra == "1111":
-            echo "Usuario autorizado, hola $nombre";
-            break;
+    switch(true):
+        case($edad <= 18) :
+        echo "Eres menor de edad";
+        break;
+        case ($edad <= 40) :
+        echo "Eres joven";
+        break;
+        case ($edad <= 65) :
+        echo "Eres maduro";
+        break;
         default:
-            echo "Usuario no autorizado";
+        echo "Cuídate";
 
 
-    }
+endswitch;
+
 }
 ?>
